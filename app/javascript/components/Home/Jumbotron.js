562cd748c574cfa01b325773e70cbd0e5301e762
Silvia Rocha <silviadprocha@gmail.com>
2020-04-19 18:59:24 +0900

    add styles components

--- app/javascript/components/Home/Jumbotron.js
+++ app/javascript/components/Home/Jumbotron.js
@@ -1,16 +1,36 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const Section = styled.section`
+  background-color: #d74234;
+  min-height: 550px;
+  padding: 100px 0;
+  color: #fff;
+`;
+
+const Header = styled.h1`
+  color: #fff;
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 52px;
+`;
+
+const Subhead = styled.p`
+  font-size: 18px;
+  font-weight: 500;
+`;
 
 class Jumbotron extends Component {
   render() {
     return (
       <div>
-        <section className="home-section--1">
+        <Section className="home-section--1">
           <div className="container">
             <div className="row">
               <div className="col col-sm-12 col-md-5">
                 <div className="pt-4 mt-4">
-                  <h1>React for Rails developers</h1>
-                  <p>Supercharge your RoR Apps with React.js</p>
+                  <Header>React for Rails developers</Header>
+                  <Subhead>Subheadercharge your RoR Apps with React.js</Subhead>
                 </div>
               </div>
               <div className="col col-sm-12 col-md-7">
@@ -27,7 +47,7 @@ class Jumbotron extends Component {
               </div>
             </div>
           </div>
-        </section>
+        </Section>
       </div>
     );
   }
