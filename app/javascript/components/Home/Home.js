13000967b9a5f15ca733474ab04e3968c2982835
Silvia Rocha <silviadprocha@gmail.com>
2020-04-19 15:32:11 +0900

    create home component

---
+++ app/javascript/components/Home/Home.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      course_modules: [
+        {
+          id: 1,
+          title: "1. Setting up a new RoR App with React.",
+          description: "lorem ipsum",
+          active: false,
+        },
+        {
+          id: 2,
+          title: "2. Adding React to an existing RoR app",
+          description: "lorem ipsum",
+          active: false,
+        },
+        {
+          id: 3,
+          title: "3. Building a Hello World App",
+          description: "lorem ipsum",
+          active: false,
+        },
+        {
+          id: 4,
+          title: "4. Adding React Router Dom to your app",
+          description: "lorem ipsum",
+          active: false,
+        },
+      ],
+    };
+  }
+
+  render() {
+    return <div>This is our Home component</div>;
+  }
+}
+
+export default Home;
